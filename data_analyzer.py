49cfd8fe88a57b4991b5da83c36c67e9c3100ad7
BranM <brandon.gmatthews@gmail.com>
2025-10-06 22:39:31 +0100

    Code clean up

--- data_analyzer.py
+++ data_analyzer.py
@@ -11,8 +11,11 @@ logger = logging.getLogger(__name__)
 
 class DataAnalyzer():
     '''Analyzes product data and generates negotiation suggestions.'''
-    
+     
     def __init__(self):
+        '''Initialize the analyzer.'''
+        logger.info("Data analyzer initialized.")
+        
         self.market_result = {}
     
     def vehicle_age(self, year: int):
@@ -20,7 +23,7 @@ class DataAnalyzer():
         current_year = datetime.now().year
         vehicle_age = current_year - year
         
-        logger.info(f"Vehicle age calculation: {current_year} - {year} = {vehicle_age} years")
+        logger.info(f"Vehicle age calculation...")
         self.market_result['vehicle_age'] = vehicle_age
     
     def analysis_vehicle_mileage(self, mileage: int):
@@ -28,17 +31,22 @@ class DataAnalyzer():
         vehicle_age = self.market_result['vehicle_age']
         
         mileage_average = round(mileage / vehicle_age)
-        logger.info(f"Vehicle mileage analysis: {mileage} miles over {vehicle_age} years = {mileage_average} miles/year")
+        logger.info(f"Vehicle mileage analysis...")
     
         if mileage_average > 12000:
             '''High mileage'''
-            print("High mileage")
+            #print("High mileage")
         elif mileage_average > 12000 and mileage_average < 10000:
             '''Average mileage'''
-            print("Average mileage")
+            #print("Average mileage")
         else:
             '''Low mileage'''
             
         self.market_result['mileage_analysis'] = mileage_average
      
+    # def analyzed_data(self):
+    #     self.vehicle_age()
+    #     self.analysis_vehicle_mileage()
+    #     return self.market_result
+
     #TODO: Add more analysis methods here as needed
